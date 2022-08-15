4ea00dc841a3cfb00889a0e98cd40d868e5d9590
SKumar <sk.kumar561@gmail.com>
2022-08-16 00:08:25 +0530

    first commit

--- my-app/src/components/GetAxiosData.js
+++ my-app/src/components/GetAxiosData.js
@@ -5,23 +5,45 @@ import { TextField, Stack, Autocomplete, Box, Button} from '@mui/material'
 function GetAxiosData() {
     const [data, setData] = useState([])
     
-    async function getData() {
-        try {
-            const result = await axios.get(`http://localhost:8000/data`)
-            // console.log(result);
-            console.log(result.data);
-            setData(result.data)
-        } catch (error) {
-            console.log(error);            
-        }
-    }
     useEffect(() => {
+        async function getData() {
+            try {
+                const result = await axios.get(`http://localhost:8000/data`)
+                // console.log(result);
+                console.log(result.data);
+                setData(result.data)
+            } catch (error) {
+                console.log(error);
+            }
+        }
         getData()
     },[])
     
   return (
       <div>
-          {data.map((item) => <p>{item.question }</p>)}
+          {/* {data.map((item) => <p>{item.question}</p>)} */}
+          <table>
+              <thead>
+                  <tr>
+                      <th>Question</th>
+                      <th>Answer</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {
+                      data &&
+                      (data).map((item, index) => {
+                          return (
+                              <tr key={index}>
+                                  <td>{item.question} </td>
+                                  <td>{item.answer}</td>
+                              </tr>
+                          )
+                      })
+                  }
+              </tbody>
+          </table>
+
     </div>
   )
 }
